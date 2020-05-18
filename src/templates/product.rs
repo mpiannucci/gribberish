b3460efaaec09c93284f99058a504207ec34e665
Matthew Iannucci <MIannucci@navatekltd.com>
2020-05-18 12:24:16 -0400

    Continue porting over enums for product type

--- src/templates/product.rs
+++ src/templates/product.rs
@@ -1,17 +1,6 @@
 use grib_data_derive::{DisplayDescription, FromValue};
 use super::template::{Template, TemplateType};
 
-pub struct Parameter {
-	name: &'static str, 
-	abbrev: &'static str,
-	unit: &'static str,
-}
-
-pub struct Product {
-	name: &'static str,
-	parameters: [Parameter],
-}
-
 #[repr(u8)]
 #[derive(Eq, PartialEq, Debug, DisplayDescription, FromValue)]
 pub enum ClusteringMethod {
@@ -97,4 +86,54 @@ pub enum TimeUnit {
 	SixHours = 9,
 	#[description = "12 hours"]
 	TwelveHours = 10,
+}
+
+#[repr(u8)]
+#[derive(Eq, PartialEq, Debug, DisplayDescription, FromValue)]
+pub enum TemperatureProduct {
+	Temperature = 0,
+	VirtualTemperature = 1,
+	PotentialTemperature = 2,
+	PseudoAdiabaticPotentialTemperature = 3,
+	MaximumTemperature = 4,
+	MinimumTemperature = 5,
+	DewpointTemperature = 6,
+	DewpointDepression = 7,
+	LapseRate = 8,
+	HeatIndex = 12, 
+	WindChillFactor = 13,
+}
+
+#[repr(u8)]
+#[derive(Eq, PartialEq, Debug, DisplayDescription, FromValue)]
+pub enum MoistureProduct {
+	SpecificHumidity = 0,
+	RelativeHUmidity = 1,
+	HumidityMixingRatio = 2,
+	PrecipitableWater = 3, 
+	Evaporation = 4,
+	PrecipitationRate = 5,
+	TotalPrecipitation = 8,
+}
+
+#[repr(u8)]
+#[derive(Eq, PartialEq, Debug, DisplayDescription, FromValue)]
+pub enum MomentumProduct {
+	WindDirection = 0,
+	WindSpeed = 1,
+	UComponentWindSpeed = 2,
+	VComponentWindSpeed = 3,
+	MaximumWindSpeed = 21,
+	WindGust = 22, 
+	UComponentWindGust = 23, 
+	VComponentWindGust = 24, 
+	WindFetch = 33, 
+}
+
+#[repr(u8)]
+#[derive(Eq, PartialEq, Debug, DisplayDescription, FromValue)]
+pub enum MassProduct {
+	Pressure = 0, 
+	PressureReducedMSL = 1, 
+	PressureTendency = 2,
 }
