882ba3bd52d4c331ef3633f2ebbaa9cac8222de4
Matthew Iannucci <mpiannucci@gmail.com>
2020-05-25 01:05:17 +0000

    Add full section enum

--- src/sections/section.rs
+++ src/sections/section.rs
@@ -5,6 +5,7 @@ use super::local_use::LocalUseSection;
 use super::grid_definition::GridDefinitionSection;
 use super::product_definition::ProductDefinitionSection;
 use super::data_representation::DataRepresentationSection;
+use super::bitmap::BitmapSection;
 use super::data::DataSection;
 use super::end::EndSection;
 
@@ -15,12 +16,17 @@ pub enum SectionType<'a> {
 	GridDefinition(GridDefinitionSection<'a>),
 	ProductDefinition(ProductDefinitionSection<'a>),
 	DataRepresentation(DataRepresentationSection<'a>),
-	Data(DataSection<'a>),
+	Bitmap(BitmapSection<'a>),
+    Data(DataSection<'a>),
 	End(EndSection<'a>),
 }
 
 fn section_length(data: &[u8], offset: usize) -> usize {
     read_u32_from_bytes(data, offset).unwrap_or(0) as usize
+}
+
+fn section_number(data: &[u8], offset: usize) -> u8 {
+    data[offset + 4]
 }
 
 pub struct Section<'a> {
@@ -30,8 +36,27 @@ pub struct Section<'a> {
 
 impl <'a> Section <'a> {
 
-	pub fn from_data(data: &'a[u8], offset: usize) {
-		let section_length = section_length(data, offset);
+	pub fn from_data(data: &'a[u8], offset: usize) -> Section<'a> {
+        let section_len = section_length(data, offset);
+        let section_num = section_number(data, offset);
+        let section_data = &data[offset..offset+section_len];
+        
+        let section = match(section_num) { 
+            0 => SectionType::Indicator(IndicatorSection::from_data(section_data)),
+            1 => SectionType::Identification(IdentificationSection::from_data(section_data)),
+            3 => SectionType::GridDefinition(GridDefinitionSection::from_data(section_data)),
+            4 => SectionType::ProductDefinition(ProductDefinitionSection::from_data(section_data)),
+            5 => SectionType::DataRepresentation(DataRepresentationSection::from_data(section_data)),
+            6 => SectionType::Bitmap(BitmapSection::from_data(section_data)),
+            7 => SectionType::Data(DataSection::from_data(section_data)),
+            8 => SectionType::End(EndSection::from_data(section_data)),
+            _ => SectionType::LocalUse(LocalUseSection::from_data(section_data)),
+        };
+
+        Section {
+            data: section_data,
+            section,
+        }
 	}
 
     pub fn length(&self) -> usize {
