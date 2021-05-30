eacb18cba48b49c15dc0866e0a5d71e17c7b2f40
Matthew Iannucci <mpiannucci@gmail.com>
2021-05-30 16:31:11 -0400

    Fix python package name

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="hello-rust",
-    version="1.0",
-    rust_extensions=[RustExtension("hello_rust.hello_rust", binding=Binding.PyO3)],
-    packages=["hello_rust"],
+    name="gribberish",
+    version="0.9.0",
+    rust_extensions=[RustExtension("gribberish.gribberish", binding=Binding.PyO3)],
+    packages=["gribberish"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
