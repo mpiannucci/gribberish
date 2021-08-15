ec914cd88d370f277796d592506c981c4a06705e
Matthew Iannucci <mpiannucci@gmail.com>
2021-08-14 21:10:41 -0400

    Save progress

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
+from setuptools_rust import RustExtension
 
 setup(
     name="gribberish",
     version="0.9.0",
-    rust_extensions=[RustExtension("gribberish.gribberish", binding=Binding.PyO3)],
+    rust_extensions=[RustExtension("gribberish.gribberish", "Cargo.toml")],
     packages=["gribberish"],
+    include_package_data=True,
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -8,9 +8,15 @@ struct GribMessage{
     inner: Message,
 }
 
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
 #[pymodule]
 fn gribberish(py: Python, m: &PyModule) -> PyResult<()> {
-    //m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
 
     Ok(())
 }
