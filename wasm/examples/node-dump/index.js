2c169a1c00bf85fa1be8b2675a2dcecefd2fcdda
Matthew Iannucci <mpiannucci@gmail.com>
2021-09-07 14:37:25 +0000

    Working node module

--- wasm/examples/node-dump/index.js
+++ wasm/examples/node-dump/index.js
@@ -1,1 +1,8 @@
 import fs from 'fs';
+import gribberish from 'gribberish-wasm';
+
+const gribData = fs.readFileSync('../../../python/examples/gfswave.20210826.t12z.atlocn.0p16.f000.grib2');
+
+const gribMessages = gribberish.parseGribMessages(gribData);
+
+console.log(`Read ${gribMessages.length} grib messages`);
