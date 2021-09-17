532bca80935a56fd7092e5125256ba230efc19e3
Matthew Iannucci <mpiannucci@gmail.com>
2021-09-17 15:49:27 -0400

    Save progress

--- node/examples/dump/index.js
+++ node/examples/dump/index.js
@@ -8,9 +8,14 @@ console.log(`Read ${gribMessages.length} grib messages`);
 console.log(`Forecast Date: ${gribMessages[0].forecastDate.toLocaleString()}`);
 
 gribMessages.forEach(gm => {
+    const location = {lat: 41.0, lon: 289.0};
+
     const data = gm.data();
-    const fortyone_seventyone_data = gm.dataAtLocation({lat: 41.0, lon: 289.0});
-    console.log(`${gm.varName} (${gm.varAbbrev}) - ${gm.units}: ${data.length} ${fortyone_seventyone_data}`);
+    const fortyone_seventyone_data = gm.dataAtLocation(location);
+    const fortyone_seventyone_index = gm.locationDataIndex(location)
+    const fortyone_seventyone_indexed_data = data[fortyone_seventyone_index];
+
+    console.log(`${gm.varName} (${gm.varAbbrev}) - ${gm.units}: ${data.length} ${fortyone_seventyone_data} ${fortyone_seventyone_indexed_data}`);
 
     // if (gm.varName === 'HTSGW') {
 
