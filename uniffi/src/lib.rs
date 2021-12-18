9dc3fa3ceb43962bd21a8b50eb9033223ac7c4c6
Matthew Iannucci <mpiannucci@gmail.com>
2021-12-18 17:50:51 -0500

    Update readme

--- uniffi/src/lib.rs
+++ uniffi/src/lib.rs
@@ -1,13 +1,27 @@
+use std::sync::Arc;
 use gribberish::message::Message;
-pub use gribberish::message::Message;
 
-pub fn parse_grib_message(data: Vec<u8>, offset: u8) -> Option<Message> {
-    None
+pub fn parse_grib_message(data: Vec<u8>, offset: u64) -> Option<Arc<GribMessage>>{
+    let message = Message::parse(data.as_slice(), offset as usize);
+    match message {
+        Ok(m) => Some(Arc::new(GribMessage{inner: m})), 
+        Err(_) => None,
+    }
 }
 
-pub fn parse_grib_messages(data: Vec<u8>) -> Option<Message> {
-    vec![]
+// pub fn parse_grib_messages(data: Vec<u8>) -> Vec<Message> {
+//     vec![]
+// }
+
+pub struct GribMessage {
+    inner: Message,
+}
+
+impl GribMessage {
+    pub fn section_count(&self) -> u64 {
+        self.inner.section_count() as u64
+    }
 }
 
 
-uniffi_macros::include_scaffolding!("grib_message");
+uniffi_macros::include_scaffolding!("gribberish");
