ed3f9ed87cf153f4a1628334bc4b41b054c61e71
Matt Iannucci <matt.iannucci@rpsgroup.com>
2022-06-28 21:36:04 -0400

    Make GribMessage struct that copies the data and takes ownership

--- src/dataset.rs
+++ src/dataset.rs
@@ -1,19 +1,51 @@
+use std::convert::TryFrom;
+use crate::message::Message;
 use std::collections::HashMap;
+use chrono::{DateTime, Utc};
 
 use crate::message::MessageIterator;
 
-pub fn map_messages<'a>(data: &'a [u8]) -> HashMap<String, usize> {
+pub fn map_messages<'a>(data: &'a [u8]) -> HashMap<String, (usize, usize)> {
     let message_iter = MessageIterator::from_data (
         data, 
         0,
     );
 
     message_iter
-        .map(|m| {
+        .enumerate()
+        .map(|(index, m)| {
             match m.variable_abbrev().or(m.parameter_index()) {
-                Ok(var) => (var, m.byte_offset()), 
-                Err(_) => ("unknown".into(), m.byte_offset())
+                Ok(var) => (var, (index, m.byte_offset())), 
+                Err(_) => ("unknown".into(), (index, m.byte_offset()))
             }
         })
         .collect()
+}
+
+pub struct GribMessage {
+    pub var: String, 
+    pub name: String, 
+    pub units: String, 
+    pub forecast_date: DateTime<Utc>,
+    pub reference_date: DateTime<Utc>, 
+    pub latitude: Vec<f64>, 
+    pub longitude: Vec<f64>,
+    pub data: Vec<f64>
+}
+
+impl <'a> TryFrom<Message<'a>> for GribMessage {
+    type Error = String;
+
+    fn try_from(message: Message) -> std::result::Result<Self, <Self as std::convert::TryFrom<Message>>::Error> { 
+        Ok(GribMessage {
+            var: message.variable_abbrev()?, 
+            name: message.variable_name()?, 
+            units: message.unit()?, 
+            forecast_date: message.forecast_date()?, 
+            reference_date: message.reference_date()?, 
+            latitude: message.latitudes()?, 
+            longitude: message.longitudes()?, 
+            data: message.data()?,
+        })
+    }
 }
