4aead6788265e99332667936f40a80a9739eced6
Matthew Iannucci <mpiannucci@gmail.com>
2023-01-06 15:38:59 -0500

    First pass unpacking complex data

--- src/templates/data_representation/complex_packing_template.rs
+++ src/templates/data_representation/complex_packing_template.rs
@@ -1,6 +1,10 @@
+use std::collections::btree_map::Values;
+
+use itertools::izip;
+
 use crate::{
     templates::template::{Template, TemplateType},
-    utils::{read_f32_from_bytes, read_i16_from_bytes, read_u32_from_bytes},
+    utils::{from_bits, read_f32_from_bytes, read_i16_from_bytes, read_u32_from_bytes, grib_power},
 };
 
 use super::{
@@ -110,6 +114,66 @@ impl DataRepresentationTemplate<f64> for ComplexPackingDataRepresentationTemplat
     }
 
     fn unpack(&self, bits: Vec<u8>, range: std::ops::Range<usize>) -> Result<Vec<f64>, String> {
-        todo!()
+        let ng = self.number_of_groups() as usize;
+        let nbits = self.bit_count() as usize;
+
+        let group_references = (0..ng).map(|ig| {
+            let start = ig * nbits;
+            let mut temp_container: [u8; 32] = [0; 32];
+            for i in 0..nbits {
+                temp_container[i] = bits[start + i];
+            }
+
+            from_bits::<u32>(&temp_container).unwrap()
+        });
+
+        let group_widths_start = ng * nbits;
+        let n_width_bits = self.group_width_bits() as usize;
+        let group_widths = (0..ng).map(|ig| {
+            let start = group_widths_start + ig * n_width_bits;
+            let mut temp_container: [u8; 32] = [0; 32];
+            for i in 0..nbits {
+                temp_container[i] = bits[start + i];
+            }
+
+            from_bits::<u32>(&temp_container).unwrap() + self.group_width_reference() as u32
+        });
+
+        let group_lengths_start = group_widths_start + n_width_bits * ng;
+        let n_length_bits = self.group_length_bits() as usize;
+        let group_lengths = (0..ng).map(|ig| {
+            let start = group_lengths_start + ig * n_length_bits;
+            let mut temp_container: [u8; 32] = [0; 32];
+            for i in 0..nbits {
+                temp_container[i] = bits[start + i];
+            }
+            
+            from_bits::<u32>(&temp_container).unwrap() * self.group_length_increment() as u32
+                + self.group_length_reference()
+        });
+
+        let bscale = grib_power(self.binary_scale_factor().into(), 2);
+        let dscale = grib_power(-(self.decimal_scale_factor() as i32), 10);
+        let reference_value: f64 = self.reference_value().into();
+
+        let mut pos = group_lengths_start + n_length_bits * ng;
+        let mut values = Vec::with_capacity(ng);
+        for (reference, width, length) in izip!(group_references, group_widths, group_lengths) {
+            let n_bits = (width * length) as usize;
+            let mut temp_container: [u8; 32] = [0; 32];
+            for i in 0..nbits {
+                temp_container[i] = bits[pos + i];
+            }
+            pos += n_bits;
+
+            let raw_value: i32 = unwrap_or_return!(
+                from_bits::<i32>(&temp_container),
+                "failed to convert value to u32".into()
+            ) + reference as i32; 
+            let value = (raw_value as f64 * bscale + reference_value) * dscale;
+            values.push(value);
+        }
+
+        Ok(values)
     }
 }
