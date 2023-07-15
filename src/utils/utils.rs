3d91f58b6bfd55f2233689ff9838f053d6274dee
Matthew Iannucci <mpiannucci@gmail.com>
2023-07-15 16:39:18 -0400

    Add tests to bit utils

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -41,16 +41,6 @@ pub fn read_f32_from_bytes(data: &[u8], offset: usize) -> Option<f32> {
     Some(f32::from_be_bytes(l))
 }
 
-// pub fn read_f64_from_bytes(data: &[u8], offset: usize) -> Option<f64> {
-//     if data.len() < offset + 8 {
-//         return None;
-//     }
-
-//     let mut l: [u8; 8] = Default::default();
-//     l.copy_from_slice(&data[offset..offset + 8]);
-//     Some(f64::from_be_bytes(l))
-// }
-
 pub fn from_bits<T>(bits: &[u8]) -> Option<T>
 where
     T: num::Unsigned + From<u8>,
@@ -86,18 +76,6 @@ pub fn bit_array_from_bytes(data: &[u8]) -> Vec<u8> {
         .collect::<Vec<u8>>()
 }
 
-// pub fn byte_to_bits(data: &u8) -> [u8; 8] {
-//     let mut result = [0; 8];
-//     let bit_string = format!("{:08b}", data);
-//     for (i, b) in bit_string.char_indices() {
-//         result[i] = b.to_digit(2).unwrap_or(0) as u8;
-//     }
-
-// pub fn positive_bit_count(data: &u8) -> u8 {
-//     let bits = byte_to_bits(data);
-//     bits.iter().sum()
-// }
-
 pub fn bits_to_bytes(bits: Vec<u8>) -> Option<Vec<u8>> {
     if bits.len() % 8 != 0 {
         return None;
@@ -114,4 +92,46 @@ pub fn bits_to_bytes(bits: Vec<u8>) -> Option<Vec<u8>> {
 #[cfg(test)]
 mod tests {
     // use super::*;
+
+    #[test]
+    fn test_from_bits() {
+        assert_eq!(super::from_bits::<u8>(&[0, 0, 0, 0, 0, 0, 0, 0]), Some(0));
+        assert_eq!(super::from_bits::<u8>(&[0, 0, 0, 0, 0, 0, 0, 1]), Some(1));
+        assert_eq!(super::from_bits::<u8>(&[0, 1, 0, 0, 0, 0, 0, 0]), Some(64));
+    }
+
+    #[test]
+    fn test_filled_bit_array() {
+        assert_eq!(super::filled_bit_array::<8>(&[1, 0, 0, 0, 0]), [0, 0, 0, 1u8, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_bit_array_from_bytes() {
+        assert_eq!(super::bit_array_from_bytes(&[128]), vec![1, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(super::bit_array_from_bytes(&[128, 64, 4]), vec![
+            1, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 1, 0, 0,
+        ]);
+    }
+
+    #[test]
+    fn test_bits_to_bytes() {
+        assert_eq!(
+            super::bits_to_bytes(vec![0, 0, 0, 0, 0, 0, 0, 0]),
+            Some(vec![0])
+        );
+        assert_eq!(
+            super::bits_to_bytes(vec![0, 0, 0, 0, 0, 0, 0, 1]),
+            Some(vec![1])
+        );
+        assert_eq!(
+            super::bits_to_bytes(vec![0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+            Some(vec![64, 1])
+        );
+        assert_eq!(
+            super::bits_to_bytes(vec![0, 1, 0, 0, 0, 0, 0, 0, 0, 1]),
+            None
+        );
+    }
 }
