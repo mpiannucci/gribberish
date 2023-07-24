25cd966dddcb2ef6eda932c2dfcf3a5047d4a680
Matthew Iannucci <mpiannucci@gmail.com>
2023-07-24 11:11:48 -0400

    Add more stats to bench

--- python/examples/bench.py
+++ python/examples/bench.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         offset = mapped[1]
         size = mapped[2].message_size
         end = offset + size
-        
+
         try:
             start = time.time()
             message = eccodes.codes_new_from_message(raw_data[offset:end])
@@ -31,7 +31,10 @@ if __name__ == '__main__':
         finally:
             eccodes.codes_release(message)
 
-    print(f'Average eccodes time: {(sum(eccodes_times) / len(eccodes_times)) * 1000} ms')
+    print(f'Mean eccodes time: {(sum(eccodes_times) / len(eccodes_times)) * 1000} ms')
+    print(f'Median eccodes time: {sorted(eccodes_times)[len(eccodes_times) // 2] * 1000} ms')
+    print(f'Max eccodes time: {max(eccodes_times) * 1000} ms')
+    print(f'Min eccodes time: {min(eccodes_times) * 1000} ms')
 
     # Then run with gribberish
     gribberish_times = []
@@ -50,4 +53,7 @@ if __name__ == '__main__':
             print(key)
             pass
     
-    print(f'Average gribberish time: {(sum(gribberish_times) / len(gribberish_times)) * 1000} ms')
+    print(f'Mean gribberish time: {(sum(gribberish_times) / len(gribberish_times)) * 1000} ms')
+    print(f'Median gribberish time: {sorted(gribberish_times)[len(gribberish_times) // 2] * 1000} ms')
+    print(f'Max gribberish time: {max(gribberish_times) * 1000} ms')
+    print(f'Min gribberish time: {min(gribberish_times) * 1000} ms')
