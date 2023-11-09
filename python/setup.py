d44b6a8de48e2ec76f00f6ba773102e9f7555761
Matthew Iannucci <mpiannucci@gmail.com>
2023-11-09 11:43:07 -0500

    Add kerchunk package to python package

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ setup(
     name="gribberish",
     version="0.10.0",
     rust_extensions=[RustExtension("gribberish.gribberishpy", debug=False)],
-    packages=["gribberish"],
+    packages=["gribberish", "gribberish.*"],
     include_package_data=True,
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
