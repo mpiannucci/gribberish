1e35224773d4c174b4db59875a55438921898e2e
Jack Kelly <jack@OpenClimateFix.org>
2024-09-25 15:44:54 +0100

    Publish the `parameters` module (#60)
    * automatic re-ordering of modules
    
    * publish parameters module

--- gribberish/src/templates/product/mod.rs
+++ gribberish/src/templates/product/mod.rs
@@ -1,11 +1,12 @@
+pub mod average_accumulation_extreme_horizontal_analysis_template;
+pub mod derived_ensemble_horizontal_analysis_template;
+pub mod derived_ensemble_horizontal_forecast_time_interval_template;
+pub mod horizontal_analysis_template;
+pub mod parameters;
 pub mod product_template;
 pub mod tables;
-pub mod horizontal_analysis_template;
-pub mod derived_ensemble_horizontal_analysis_template;
-pub mod average_accumulation_extreme_horizontal_analysis_template;
-pub mod derived_ensemble_horizontal_forecast_time_interval_template;
-mod parameters;
 
-pub use horizontal_analysis_template::HorizontalAnalysisForecastTemplate;
-pub use derived_ensemble_horizontal_analysis_template::DerivedEnsembleHorizontalAnalysisForecastTemplate;
 pub use average_accumulation_extreme_horizontal_analysis_template::AverageAccumulationExtremeHorizontalAnalysisForecastTemplate;
+pub use derived_ensemble_horizontal_analysis_template::DerivedEnsembleHorizontalAnalysisForecastTemplate;
+pub use horizontal_analysis_template::HorizontalAnalysisForecastTemplate;
+
