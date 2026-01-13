b62348d08418df18d68bb285f9c2ca194b55ce04
Vladislav Wohlrath <vladislav.wohlrath@second-foundation.eu>
2026-01-13 19:20:49 +0100

    fix: support zarr 3.1 refactored dtypes

--- python/tests/test_zarr_codec.py
+++ python/tests/test_zarr_codec.py
@@ -5,10 +5,16 @@ import numpy as np
 zarr = pytest.importorskip("zarr")
 
 
-async def test_decode_data_var_gribberish():
+@pytest.mark.parametrize(
+    'dtype_str',
+    ['float64', 'float32'],
+)
+async def test_decode_data_var_gribberish(dtype_str):
     from gribberish.zarr.codec import GribberishCodec
-    from zarr.core.array_spec import ArraySpec
+    from zarr.core.array_spec import ArraySpec, ArrayConfig
     from zarr.core.buffer import default_buffer_prototype
+    from zarr.dtype import parse_dtype
+    from zarr.buffer.cpu import buffer_prototype
 
     with open("./../gribberish/tests/data/hrrr.t06z.wrfsfcf01-UGRD.grib2", "rb") as f:
         raw_data = f.read()
@@ -19,15 +25,15 @@ async def test_decode_data_var_gribberish():
         buffer,
         ArraySpec(
             shape=(1059, 1799),
-            dtype="float64",
+            dtype=parse_dtype(dtype_str, zarr_format=3),
             fill_value=0,
-            order="C",
-            prototype=np.ndarray,
+            prototype=buffer_prototype,
+            config=ArrayConfig(order='C', write_empty_chunks=False),
         ),
     )
 
     assert data.shape == (1059, 1799)
-    assert data.dtype == np.dtype("float64")
+    assert data.dtype == np.dtype(dtype_str)
     (
         np.testing.assert_almost_equal(data[0][1000], -4.46501350402832),
         "Data not decoded correctly",
